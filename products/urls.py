edfe415fd16a9a617c6addcc3014c67699b4dbca
MihaiElisei <mihai.m.elisei@gmail.com>
2023-04-29 20:43:28 +0000

    added category template and view

---
+++ products/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_products, name='products'),
+    path('<slug:category_slug>/', views.all_products, name='products_by_category'),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 # Create your views here.
+
+
+def all_products(request, category_slug=None):
+    """ A view to show all products, including sorting and search queries """
+
+    categories = None
+    products = None
+
+    if category_slug is not None:
+        categories = get_object_or_404(
+            Category, slug=category_slug
+            )
+        products = Product.objects.filter(
+            category=categories, is_available=True
+            )
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
+
+    context = {
+        'products': products,
+        'products_count': product_count,
+    }
+    return render(request, 'products/products.html', context)

0deea7a02ab9f79ba0cf376c20989c1b27e16e48
Mihai <mihai.m.elisei@gmail.com>
2023-04-30 10:49:42 +0100

    added cart models

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def view_cart(request):
     """ A view that renders the cart contents page """
-    
+
     return render(request, 'cart/cart.html')
